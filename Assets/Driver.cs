e0c51d981c2fd18d12a2a3ffa0f2b600da361e95
unknown <paulo.henriqq8@gmail.com>
2025-09-18 23:46:09 -0400

    Add basic car movement

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float moveSpeed = 0.5f;
+    [SerializeField] float steerSpeed = 0.5f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        float move = 0f;
+        float steer = 0f;
+
+
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move = 1f;
+        }
+
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            move = -1f;
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            steer = 1f;
+        }
+
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            steer = -1f;
+        }
+
+        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float steerAmount = steer * steerSpeed * Time.deltaTime;
         
+        transform.Translate(0, moveAmount, 0);
+        transform.Rotate(0, 0, steerAmount);
     }
 }
