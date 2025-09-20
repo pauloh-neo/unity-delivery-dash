b5ecbafccf53cc3c6f1b16c5f7294ba2297ff016
unknown <paulo.henriqq8@gmail.com>
2025-09-19 21:53:15 -0400

    Feature Boosts and Bumps

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -3,14 +3,26 @@ using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 0.5f;
-    [SerializeField] float steerSpeed = 0.5f;
-    void Start()
+    [SerializeField] float currentSpeed = 5f;
+    [SerializeField] float steerSpeed = 200f;
+    [SerializeField] float boostSpeed = 10f;
+    [SerializeField] float regularSpeed = 5f;
+
+    [SerializeField] float delay = 0.3f;
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Boost"))
+        {
+            currentSpeed = boostSpeed;
+            Destroy(collision.gameObject, delay);
+        }
     }
 
-    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentSpeed = regularSpeed;
+    }
     void Update()
     {
         float move = 0f;
@@ -37,7 +49,7 @@ public class Driver : MonoBehaviour
             steer = -1f;
         }
 
-        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float moveAmount = move * currentSpeed * Time.deltaTime;
         float steerAmount = steer * steerSpeed * Time.deltaTime;
         
         transform.Translate(0, moveAmount, 0);
