88dee6b5c98f24a2e8aa3a03e70ab24630c7aa0b
unknown <paulo.henriqq8@gmail.com>
2025-09-19 21:27:14 -0400

    Fix Picked up logic

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    bool hasPackage;
+    [SerializeField] bool hasPackage;
+    [SerializeField] float delay = 0.5f;
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Package"))
+
+        if (collision.CompareTag("Package") && !hasPackage)
         {
             Debug.Log("Picked up package!");
             hasPackage = true;
+            Destroy(collision.gameObject, delay);
 
         }
         else if (collision.CompareTag("Customer") && hasPackage)
